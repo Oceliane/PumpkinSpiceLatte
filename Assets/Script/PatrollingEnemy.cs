f3ae64075c91bdba6f2f286508609b51bee07d28
Stronius <61824330+Stronius@users.noreply.github.com>
2022-09-07 09:25:39 +0200

    Enemy (no movements)

--- Assets/Script/PatrollingEnemy.cs
+++ Assets/Script/PatrollingEnemy.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PatrollingEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform[] patrolPositions;
+    [SerializeField] int startingPos;
+    int currentPos;
+    int targetPos;
+    [SerializeField] bool loops;
+    [SerializeField] float moveSpeed;
+    [SerializeField] Color lineColor;
+
+    private void Start()
     {
-        
+        startingPos = Random.Range(0, patrolPositions.Length);
+        transform.position = patrolPositions[startingPos].position;
+        currentPos = startingPos;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Vector3 moveDir = (transform.position - patrolPositions[currentPos].position).normalized;
+        transform.position += moveDir * moveSpeed;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = lineColor;
+        if (patrolPositions.Length > 1)
+        {
+            for (int i = 0; i < patrolPositions.Length - 1; i++)
+            {
+                Gizmos.DrawLine(patrolPositions[i].position, patrolPositions[i + 1].position);
+            }
+        }    
         
+        if (loops)
+            Gizmos.DrawLine(patrolPositions[patrolPositions.Length-1].position, patrolPositions[0].position);
     }
 }
