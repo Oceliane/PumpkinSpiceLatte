cc110f3890bda0b82f68c74683d76f566e2c80c3
Stronius <61824330+Stronius@users.noreply.github.com>
2022-09-09 10:31:06 +0200

    Death screen

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,17 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    #region Instance
+    static GameManager instance = null;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    [SerializeField] GameObject deathScreen;
+
     private void Awake()
     {
         Application.targetFrameRate = 50;
         Debug.Log(Application.targetFrameRate + " fps");
     }
 
-    public void PlayerHidden()
+    public void PlayerDeath()
     {
+        if (!deathScreen.activeInHierarchy)
+        {
+            deathScreen.SetActive(true);
+            Invoke("RestartGame", 4);
+        }
+    }
 
+    void RestartGame()
+    {
+        SceneManager.LoadScene("LevelTest");
     }
 }
